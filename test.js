7128220eb09891f6c647598acabcf2039102d235
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-12-01 14:57:22 +0100

    Update npm dependencies

--- test.js
+++ test.js
@@ -1,50 +1,53 @@
 require('jsdom-global')()
 
 const assert = require('assert')
-const body = document.body
 const { loadImage, loadImages } = require('./')
-const pathGen = pathGenerator()
 
+
+const LOAD_FAILURE_SRC = 'succes.jpg'
+const LOAD_SUCCESS_SRC = 'failed.jpg'
 
 /* eslint-disable */
-function * pathGenerator() {
-  let i = 1
-  while (i) {
-    yield `data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7?${i++}`
+Object.defineProperty(global.Image.prototype, 'src', {
+  set(src) {
+    if (src === LOAD_FAILURE_SRC) {
+      setTimeout(() => this.onerror(new Error('mocked error')), Math.random() * 1000)
+    } else if (src === LOAD_SUCCESS_SRC) {
+      setTimeout(() => this.onload(), Math.random() * 1000)
+    }
   }
-}
+})
 /* eslint-enable */
 
-describe('Bianco images-loader', function() {
-  beforeEach(function() {
-    const div = document.createElement('div')
-    div.innerHTML = `
-      <img src=''>
-    `
-    body.appendChild(div)
-  })
 
+describe('Bianco images-loader', function() {
   it('It can load images in the DOM', function(done) {
-    const img = document.querySelector('img')
+    const img = document.createElement('img')
+
     loadImage(img).then(function(i) {
-      assert.equal(typeof i.src, 'string')
+      assert.ok(i)
       done()
     })
-    img.src = pathGen.next().value
+
+    img.src = LOAD_SUCCESS_SRC
   })
 
   // this test does not work in jsdom somehow
-  /*it('It can throw properly the errors', function(done) {
-    const img = document.querySelector('img')
-    loadImage(img).catch(function(e) {
-      assert.equal(typeof e, Error)
+  it('It can throw properly the errors', function(done) {
+    const img = document.createElement('img')
+
+    loadImage(img).then(() => {
+      throw 'This image should be not loaded'
+    }).catch(function(e) {
+      assert.ok(e instanceof Error)
       done()
     })
-    img.src = 'foo-bar'
-  })*/
+
+    img.src = LOAD_FAILURE_SRC
+  })
 
   it('It can load arrays of images urls', function(done) {
-    loadImages([pathGen.next().value, pathGen.next().value]).then(function(imgs) {
+    loadImages([LOAD_SUCCESS_SRC, LOAD_SUCCESS_SRC]).then(function(imgs) {
       assert.equal(imgs.length, 2)
       done()
     })
